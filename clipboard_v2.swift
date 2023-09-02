8ac16e34a98e53afa9021c5e14ad0fb3c0832a01
Deepak Bhetwal <deepak.bhetwal@fairfaxmedia.com.au>
2023-09-02 19:24:45 +1000

    remembers last 5 copy

--- clipboard_v2.swift
+++ clipboard_v2.swift
@@ -2,10 +2,14 @@ import Cocoa
 
 class ClipboardMonitor {
     let pasteboard = NSPasteboard.general
+
     var changeCount = NSPasteboard.general.changeCount
+
     let outputPath = "clipboard_history.txt"
-    var previous1 = ""
-    var previous2 = ""
+
+    var recentInputs = [String](repeating: "", count: 5)
+
+    var currentIndex = 0
 
     init() {
         createFileIfNotExists()
@@ -17,21 +21,25 @@ class ClipboardMonitor {
 
     func checkClipboardChange() {
         let newChangeCount = pasteboard.changeCount
+
         if newChangeCount != changeCount {
+
             changeCount = newChangeCount
+
             if let content = getClipboardContent() {
-                if previous1.trimmingCharacters(in: .whitespacesAndNewlines) == content.trimmingCharacters(in: .whitespacesAndNewlines) || previous2.trimmingCharacters(in: .whitespacesAndNewlines) == content.trimmingCharacters(in: .whitespacesAndNewlines) || content.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
-                return
+                if !recentInputs.contains(content) {
+
+                    recentInputs[currentIndex] = content
+
+                    currentIndex = (currentIndex + 1) % 5
+
+                    print("\u{001B}[31;1m--------------------Clipboard you copied--------------------\u{001B}[0m")
+
+                    print("\u{001B}[32m\(content)\u{001B}[0m")
+
+                    writeToFile(content:content)
+                    
                 }
-                print("\u{001B}[31;1m--------------------Clipboard you copied--------------------\u{001B}[0m")
-                print("\u{001B}[32m\(content)\u{001B}[0m")
-                print("------------------------------------------------------------")
-
-                writeToFile(content:content)
-
-                previous2 = previous1
-                previous1 = content
-
             }
         }
     }
@@ -43,17 +51,22 @@ class ClipboardMonitor {
     }
 
     func getClipboardContent() -> String? {
-        return pasteboard.string(forType: .string)
+        return pasteboard.string(forType: .string)!.trimmingCharacters(in: .whitespacesAndNewlines)
     }
 
     func writeToFile(content: String) {
         do {
             let fileHandle = try FileHandle(forWritingTo: URL(fileURLWithPath: outputPath))
+
             fileHandle.seekToEndOfFile()
+
             let bullet = " • "
+
             let line = "\n\n\(bullet)\(content)"
+
             if let data = line.data(using: .utf8) {
                 fileHandle.write(data)
+
                 fileHandle.closeFile()
             }
         } catch {
@@ -64,4 +77,5 @@ class ClipboardMonitor {
 }
 
 let _ = ClipboardMonitor()
+
 RunLoop.current.run()
