296a785c7aa6239d38462687c0f59d65ec7c87af
Deepak Bhetwal <deepak.bhetwal@fairfaxmedia.com.au>
2023-09-03 10:08:41 +1000

    updated install script

--- install.sh
+++ install.sh
@@ -10,19 +10,29 @@ DESTINATION="/Users/$USERNAME"
 REPO_URL="https://github.com/Razz99/clipboard.git"
 
 # Alias command to add to .zshrc
-ALIAS_COMMAND="alias clip='swift $DESTINATION/clipboard/clipboard_v2.swift"
+ALIAS_COMMAND="alias clip='swift $DESTINATION/clipboard/clipboard_v2.swift'"
 
-# Check if the destination directory exists
-if [ ! -d "$DESTINATION" ]; then
-  echo "Error: User/$USERNAME Directory for user $USERNAME does not exist."
+# Prompt the user for the path to the .zshrc file
+echo "Please enter the path to your .zshrc file\n(Most of the time path would be ==> /Users/$USERNAME/.zshrc)"
+read ZSHRC_PATH
+
+# Check if the specified .zshrc file exists
+if [ ! -f "$ZSHRC_PATH" ]; then
+  echo "Error: .zshrc file does not exist in the specified path."
   exit 1
 fi
 
 # Change directory to the user's desktop
 cd "$DESTINATION" || exit 1
 
-# Clone the repository
-git clone "$REPO_URL"
+# Check if the repository directory already exists, and if so, delete it
+if [ -d "clipboard" ]; then
+  rm -rf "clipboard"
+fi
+
+# Clone the repository with a spinner animation
+echo "Installing........"
+git clone --quiet "$REPO_URL"
 
 # Check if the clone was successful
 if [ $? -ne 0 ]; then
@@ -30,14 +40,9 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-# Add the alias command to the system-wide .zshrc
-if [ "$(id -u)" -eq 0 ]; then
-  ZSHRC_FILE="/etc/zsh/zshrc"
-else
-  ZSHRC_FILE="/etc/zsh/zshrc"
-fi
-
-echo "$ALIAS_COMMAND" >> "$ZSHRC_FILE"
+# Add the alias command to the specified .zshrc file
+echo "$ALIAS_COMMAND" >> "$ZSHRC_PATH"
+echo "Done!"
 
 # Inform the user about the added alias
-echo "New Clipboard for your MAC is ready to use. Type clip in your terminal and it is ready to remember everything you copied. Recommended to use with oh-my-zsh terminal."
+echo "\n\nNew Clipboard for your MAC is ready to use. Type 'clip' in your terminal, and it is ready to remember everything you copied. Recommended to use with oh-my-zsh terminal."
